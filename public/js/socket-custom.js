96df5fce9e9ceb937ce3be9691fefb1b69ecea59
Gustavo <Gustavomarianodelgado@gmail.com>
2018-10-20 21:00:03 -0300

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,24 @@
+var socket = io();
+
+socket.on('connect',
+
+    function() {
+        console.log('Conectado al servidor');
+    });
+//escuchar    
+socket.on('disconnect', function() {
+    console.log('Perdimos conexion con el servidor');
+});
+
+//Enviar informacion
+socket.emit('enviarMensaje', {
+    usuario: 'Fernando',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});
+
+//Escuchar Informacion
+socket.on('enviarMensaje', function(mensaje) {
+    console.log('Servidor: ', mensaje);
+});
